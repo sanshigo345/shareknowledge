3904dbae066ba193649c66b747a5812c5c904c98
Buğra Ercan <38170967+sanshigo345@users.noreply.github.com>
2024-03-28 21:04:32 +0300

    Update App.jsx

--- src/shareknowledge_frontend/src/App.jsx
+++ src/shareknowledge_frontend/src/App.jsx
@@ -2,28 +2,64 @@ import { useState } from 'react';
 import { shareknowledge_backend } from 'declarations/shareknowledge_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [bookTitle, setBookTitle] = useState('');
+  const [bookList, setBookList] = useState([]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    shareknowledge_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
+  const onBookTitleChange = (event) => {
+    setBookTitle(event.target.value);
+  }
+
+  function isBookInList(bookTitle) {
+    for (const book of bookList) {
+      if (book.title === bookTitle) {
+        return true;
+      }
+    }
     return false;
+  }
+
+  const addBookHandler = async () => {
+
+    if (bookTitle === '' && isBookInList(bookTitle)) {
+      alert('Please enter a unique book title');
+    } else {
+      await shareknowledge_backend.addBook(bookTitle, true);
+      getBookList();
+    };
+    setBookTitle('');
+  }
+
+  const getBookList = async () => {
+    await shareknowledge_backend.getBooks().then((books) => {
+      setBookList(books.reverse());
+      console.log(books);
+    })
+  }
+
+  const handleBorrow = async (index) => {
+    await shareknowledge_backend.borrowBook(index);
+    getBookList();
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <label htmlFor="title">Enter your book title: </label> <br />
+      <input id="title" alt="title" type="text" onChange={onBookTitleChange} />
+      <button onClick={addBookHandler}>Add this Book to Total Collection!</button>
+      <div>
+        <ul>
+          {bookList.map((book, index) => (
+            <li key={index}>
+              {book.title}
+              {book.available ? (
+                <button onClick={() => handleBorrow(index)}>Borrow this book</button>
+              ) : (
+                <button disabled>Book already borrowed by someone</button>
+              )}
+            </li>
+          ))}
+        </ul>
+      </div>
     </main>
   );
 }
